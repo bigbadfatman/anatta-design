996b7c7515f3697730a48b8dc79b311fc40ca274
Ashfaque Ahmad Bari <ashfaqueahmadbari@gmail.com>
2019-08-04 05:14:44 +0530

    added baseroute

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={`${process.env.NODE_ENV === 'development' ? "" : "/anatta-design"}`}>
         <Header/>
         <ProductPage/>
         <Footer/>
